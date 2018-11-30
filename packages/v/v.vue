ac987ebc040408fb3440a3000cb5f324f0ea4ff3
leafiy <t@leafiy.com>
2018-12-01 02:51:28 +0800

    v dev in progress

--- packages/v/v.vue
+++ packages/v/v.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="ui-v"><slot></slot></div>
+  <div class="ui-v">
+    <slot></slot>
+  </div>
 </template>
-
 <script>
 export default {
   name: "ui-v",
 
   data() {
-    return {};
+    return {
+      fields: []
+    };
+  },
+  props: {
+    rules: Array
+  },
+  methods: {
+    handleInput(e) {
+      console.log(e)
+    },
+    handleBlur(e) { console.log(e) },
+    handleInput(e) { console.log(e) },
+    handleFocus(e) { console.log(e) },
+    handleKeyup(e) { console.log(e) },
+    handleKeydown(e) { console.log(e) },
+    parseFileds() {
+      this.$slots.default.forEach(field => {
+        if (field.componentInstance && !field.componentInstance.$el.querySelector('input').name) {
+          return new Error('input or component must have a name')
+        }
+        // field.componentInstance.handleInput = this.handleInput
+        // field.componentInstance.handleBlur = this.handleBlur
+        // field.componentInstance.handleInput = this.handleInput
+        // field.componentInstance.handleFocus = this.handleFocus
+        // field.componentInstance.handleKeyup = this.handleKeyup
+        // field.componentInstance.handleKeydown = this.handleKeydown
+        this.fields.push(field.componentInstance)
+
+      })
+    }
   },
   mounted() {
-    console.log(this.$slots);
+    if (this.$slots.default.length) {
+      this.parseFileds()
+    } else {
+      throw new Error('why?')
+    }
   }
 };
-</script>
 
+</script>
 <style lang="css" scoped></style>
