41152144d91b95f7bdd321143ff3e6da8bd68741
leafiy <t@leafiy.com>
2019-01-02 13:29:38 +0800

    fix(style fix):

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   title: 'Gelatin-Ui',
   description: '🎨 简单好看的VueJs UI from unicapsule.com',
   port: 8108,
-  host: "192.168.145.132",
+  host: "192.168.228.128",
   base: "/",
   themeConfig: {
     displayAllHeaders: true,
